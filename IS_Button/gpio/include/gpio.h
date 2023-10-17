4571a842c5650a920dcdc1b360273333c69ba8b2
Metisai <lequynga12345@gmail.com>
2023-10-17 20:11:24 +0700

    First commit

---
+++ IS_Button/gpio/include/gpio.h
@@ -0,0 +1,4 @@
+
+#include <stdio.h>
+#include "driver/gpio.h"
+void GpioInit(gpio_num_t number, gpio_mode_t mode);
---
+++ IS_Button/main/IS_Button.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <gpio.h>
+
+#define BUTTON_1 
+#define BUTTON_2 
+#define BUTTON_3
+
+
+#define RELAY_1
+#define RELAY_2
+#define RELAY_3
+
+void app_main(void)
+{
+    GpioInit(gpio_num_t number, gpio_mode_t mode);
+}
+
